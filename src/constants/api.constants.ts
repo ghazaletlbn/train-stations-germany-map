4e300609ea496b4157e5389f98bed263e334043b
ghazaletlbn <ghazal7988@gmail.com>
2026-02-16 12:37:31 +0330

    feat(constants): add API and map configuration constants

---
+++ src/constants/api.constants.ts
@@ -0,0 +1,12 @@
+export const API_CONFIG = {
+    STATIONS_URL:
+        'https://gist.githubusercontent.com/neysidev/bbd40032f0f4e167a1e6a8b3e99a490c/raw',
+    TIMEOUT: 10000,
+    RETRY_ATTEMPTS: 3,
+} as const;
+
+export const MAP_CONFIG = {
+    DEFAULT_CENTER: { lat: 51.1657, lng: 10.4515 }, // Germany center
+    DEFAULT_ZOOM: 6,
+    MARKER_ZOOM: 12,
+} as const;
